7fabced42b97ef4f0311d25c587d0f9c3c4bb6b2
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2024-07-16 13:03:57 +0530

    practice

--- 04BuildingHttpserver/index.js
+++ 04BuildingHttpserver/index.js
@@ -1,6 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const mysever = http.createServer((req, res) => {
+   const log  = `${Date.now()} : new request received \n`;
+    fs.appendFile('log.txt'. log);
     console.log("New request received");
     console.log(req);
     res.end("Hello from server");
